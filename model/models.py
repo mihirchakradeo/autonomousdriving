adea94eddc9140b5c37bc1fa203293651aa4b9fa
bhushan <bhushan.s.94@gmail.com>
2018-12-05 21:44:11 -0500

    Base model

---
+++ model/models.py
@@ -0,0 +1,44 @@
+import torch
+import torch.nn as nn
+
+class base_model(nn.Module):
+    def __init__(self):
+        super(base_model, self).__init__()
+        self.conv_net = nn.Sequential(
+            nn.Conv2d(3, 24, 5, stride = 2, bias = False),
+            nn.ReLU(),
+
+            nn.Conv2d(24, 36, 5, stride = 2, bias = False),
+            nn.ReLU(),
+            nn.BatchNorm2d(36),
+
+            nn.Conv2d(36, 48, 5, stride = 2, bias = False),
+            nn.ReLU(),
+            nn.BatchNorm2d(48),
+
+
+            nn.Conv2d(48, 64, 5, stride = 2, bias = False),
+            nn.ReLU(),
+            nn.BatchNorm2d(64),
+
+            nn.Conv2d(64, 64, 5, stride = 2, bias = False),
+            nn.ReLU(),
+            nn.Dropout(p=0.35)
+        )
+
+        self.linear_net = nn.Sequential(
+            nn.Linear(64 * 1 * 18, 100, bias = False),
+            nn.ReLU(),
+            nn.Linear(100, 50, bias = False).
+            nn.ReLU(),
+            nn.Linear(50, 10, bias = False).
+            nn.ReLU(),
+            nn.Linear(10, 1, bias = False)
+        )
+
+    def forward(self, x):
+        x = self.conv_net(x)
+        x = x.view(x.shape(0), 64 * 1 * 18)
+        x = self.linear_net(x)
+        return x
+
